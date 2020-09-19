235542162bec2cd38940f1d1e587915bff00a28e
Tom Chen <fadeawaygod@gmail.com>
2020-09-19 22:22:32 +0800

    perf: add recursion to removeGoogleAlertWindow

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <div id="app">
     <div id="map" ref="map"></div>
+    <button>post a party</button>
+    <modal ref="modalName">
+      <template v-slot:header>
+        <h1>Modal title</h1>
+      </template>
+
+      <template v-slot:body>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed velit dignissim sodales ut eu sem integer vitae. Id aliquet lectus proin nibh nisl condimentum. Fringilla urna porttitor rhoncus dolor purus. Nam aliquam sem et tortor. Nisl vel pretium lectus quam id. Cras pulvinar mattis nunc sed. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Tristique magna sit amet purus. Fermentum et sollicitudin ac orci phasellus egestas tellus. Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan. Felis eget nunc lobortis mattis aliquam faucibus. Tincidunt eget nullam non nisi est sit amet facilisis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Vitae proin sagittis nisl rhoncus mattis rhoncus urna neque. Eget nunc scelerisque viverra mauris in aliquam sem fringilla ut. Nec nam aliquam sem et tortor consequat id. Commodo nulla facilisi nullam vehicula ipsum a. Elementum tempus egestas sed sed. Faucibus purus in massa tempor nec feugiat nisl pretium fusce.</p>
+        <p>Arcu cursus vitae congue mauris rhoncus aenean. Tempor id eu nisl nunc mi. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Ut faucibus pulvinar elementum integer enim. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Eu non diam phasellus vestibulum lorem sed risus. Porttitor rhoncus dolor purus non enim praesent. Sit amet mauris commodo quis imperdiet. Lobortis feugiat vivamus at augue eget. Nibh tellus molestie nunc non blandit. Tellus mauris a diam maecenas sed enim ut. Tortor aliquam nulla facilisi cras fermentum odio eu feugiat pretium. Mattis aliquam faucibus purus in massa.</p>
+      </template>
+
+      <template v-slot:footer>
+        <div>
+          <button @click="$refs.modalName.closeModal()">Cancel</button>
+          <button @click="$refs.modalName.closeModal()">Save</button>
+        </div>
+      </template>
+    </modal>
   </div>
 </template>
 
 <script>
-import simulate from "@/tools/simulate"
+import simulate from "@/tools/simulate";
+import Modal from "@/components/modal";
 const MAP_CENTER_COORIDINATE = { lat: 25.039, lng: 121.512 };
 const DEFULAT_MAP_ZOOM = 14;
 
 export default {
   name: "App",
+  components: {
+    Modal
+  },
   data() {
     return {
       map: {},
@@ -25,31 +47,34 @@ export default {
     });
     this.refreshMarkers();
     this.removeGoogleAlertWindow();
-   
   },
-  methods:{
-    refreshMarkers(){
+  methods: {
+    refreshMarkers() {
       for (let marker of this.markers) {
         let googleMapMarker = new window.google.maps.Marker({
           position: marker.cooridinate,
           map: this.map,
-          label:"🏸",
+          label: "🏸",
           animation: window.google.maps.Animation.DROP,
-          title: 'Hello World!'
+          title: "Hello World!"
         });
-        googleMapMarker.addListener('click', ()=>alert('test'));
+        googleMapMarker.addListener("click", () => alert("test"));
         this.googleMapMarkers.push(googleMapMarker);
-      } 
-    
-
+      }
     },
-    removeGoogleAlertWindow(){
-      setTimeout(()=>{
-      simulate(document.getElementsByClassName("dismissButton")[0], "click");  
-    }, 800);
+    removeGoogleAlertWindow(count = 0) {
+      setTimeout(() => {
+        if (
+          document.getElementsByClassName("dismissButton").length === 0 &&
+          count < 40
+        ) {
+          this.removeGoogleAlertWindow();
+          return;
+        }
+        simulate(document.getElementsByClassName("dismissButton")[0], "click");
+      }, 50);
     }
   }
-
 };
 </script>
 
